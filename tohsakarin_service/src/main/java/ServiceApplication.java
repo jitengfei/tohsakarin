e6250f0f556c5b8cdf2bd272281c3cc16068a6b5
jtf_fly <1510266928@qq.com>
2017-09-21 17:37:54 +0800

    service application

---
+++ tohsakarin_service/src/main/java/ServiceApplication.java
@@ -0,0 +1,15 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(ServiceApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class,args);
+    }
+
+}
