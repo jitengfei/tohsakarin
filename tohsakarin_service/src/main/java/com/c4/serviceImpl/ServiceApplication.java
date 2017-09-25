7584435a8f9defb3699cee6940a1198a0494b423
jtf_fly <1510266928@qq.com>
2017-09-25 11:40:28 +0800

    service 配置

--- tohsakarin_service/src/main/java/com/c4/serviceImpl/ServiceApplication.java
+++ tohsakarin_service/src/main/java/com/c4/serviceImpl/ServiceApplication.java
@@ -1,3 +1,5 @@
+package com.c4.serviceImpl;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -9,6 +11,7 @@ public class ServiceApplication {
     private static final Logger logger = LoggerFactory.getLogger(ServiceApplication.class);
 
     public static void main(String[] args) {
+        logger.info("Service 开始启动");
         SpringApplication.run(ServiceApplication.class,args);
         logger.info("Service 启动完成");
     }
